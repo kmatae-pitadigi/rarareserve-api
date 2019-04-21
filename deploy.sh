69b5e8751c9e2522cf946983dd4d6ad0690f5840
俣江 幸治 <kmatae@pitadigi.jp>
2019-04-21 17:32:36 +0900

    Azure CLIで確認したものを反映

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,27 @@
 #! /bin/bash
 
-# Resource Group name
+# リソースグループ名
 RES_GROUP=$1
-# Azure Container Registry registry name
+# Azureコンテナーレジストリ名
 ACR_NAME=$2
-# Azure Key Vault vault name
+# Azureキーコンテナ名
 AKV_NAME=$2
-# Azure Container Instance Name
-CONTAINER_NAME=$1-api-$3
+# Azureコンテナーレジストリサーバー名
+ACR_LOGIN_SERVER=$(az acr show --name $ACR_NAME --resource-group $2 --query "loginServer" --output tsv)
+# Azureコンテナーレジストリリポジトリ名
+REPOSITORY_NAME=$1-api
+# Azureコンテナーインスタンス名
+CONTAINER_NAME=$REPOSITORY_NAME-$3
 
-# Azure Container Registry Server Name
-ACR_LOGIN_SERVER=$(sudo az acr show --name $ACR_NAME --resource-group $2 --query "loginServer" --output tsv)
-
-# Create Azure Container Instance
-sudo az container create \
+# AzureコンテナーインスタンスのDockerイメージを更新する
+az container create \
     --name $CONTAINER_NAME \
     --resource-group $RES_GROUP \
     --location japaneast \
     --image $ACR_LOGIN_SERVER/$REPOSITORY_NAME:$3 \
     --registry-login-server $ACR_LOGIN_SERVER \
-    --registry-username $(sudo az keyvault secret show --vault-name $AKV_NAME -n $ACR_NAME-pull-usr --query value -o tsv) \
-    --registry-password $(sudo az keyvault secret show --vault-name $AKV_NAME -n $ACR_NAME-pull-pwd --query value -o tsv) \
+    --registry-username $(az keyvault secret show --vault-name $AKV_NAME -n $ACR_NAME-pull-usr --query value -o tsv) \
+    --registry-password $(az keyvault secret show --vault-name $AKV_NAME -n $ACR_NAME-pull-pwd --query value -o tsv) \
     --dns-name-label $CONTAINER_NAME \
     --query ipAddress.fqdn \
     --os-type Linux \
