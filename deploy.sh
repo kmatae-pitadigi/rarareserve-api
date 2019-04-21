7d0052a079c1535eb91905e97bda5ebf6f4427c5
俣江 幸治 <kmatae@pitadigi.jp>
2019-04-21 16:59:54 +0900

    sudoを追加

--- deploy.sh
+++ deploy.sh
@@ -6,20 +6,21 @@ RES_GROUP=$1
 ACR_NAME=$2
 # Azure Key Vault vault name
 AKV_NAME=$2
-# Azure Container Registry Server Name
-ACR_LOGIN_SERVER=$(az acr show --name $ACR_NAME --resource-group $2 --query "loginServer" --output tsv)
 # Azure Container Instance Name
 CONTAINER_NAME=$1-api-$3
 
+# Azure Container Registry Server Name
+ACR_LOGIN_SERVER=$(sudo az acr show --name $ACR_NAME --resource-group $2 --query "loginServer" --output tsv)
+
 # Create Azure Container Instance
-az container create \
+sudo az container create \
     --name $CONTAINER_NAME \
     --resource-group $RES_GROUP \
     --location japaneast \
     --image $ACR_LOGIN_SERVER/$REPOSITORY_NAME:$3 \
     --registry-login-server $ACR_LOGIN_SERVER \
-    --registry-username $(az keyvault secret show --vault-name $AKV_NAME -n $ACR_NAME-pull-usr --query value -o tsv) \
-    --registry-password $(az keyvault secret show --vault-name $AKV_NAME -n $ACR_NAME-pull-pwd --query value -o tsv) \
+    --registry-username $(sudo az keyvault secret show --vault-name $AKV_NAME -n $ACR_NAME-pull-usr --query value -o tsv) \
+    --registry-password $(sudo az keyvault secret show --vault-name $AKV_NAME -n $ACR_NAME-pull-pwd --query value -o tsv) \
     --dns-name-label $CONTAINER_NAME \
     --query ipAddress.fqdn \
     --os-type Linux \
