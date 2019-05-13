a3774c2ba557f682944184fe320219a6147a2889
kmatae-pitadigi <kmatae@pitadigi.jp>
2019-05-13 17:10:58 +0900

    ormconfigを修正

--- ormconfig.js
+++ ormconfig.js
@@ -8,7 +8,7 @@ module.exports = [
         "database": process.env.DATABASE_DATABASE,
         "extra": {
             "options": {
-                "encrypt": process.env.DATABASE_ENCRYPY === "yes" ? true: false
+                "encrypt": process.env.DATABASE_ENCRYPY === 'yes'
             }
         },
         "synchronize": false,
