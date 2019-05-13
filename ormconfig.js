54d163414c81d344edfd608c96477cd4f0afd0c6
kmatae-pitadigi <kmatae@pitadigi.jp>
2019-05-13 17:06:23 +0900

    ormconfigのportを修正

--- ormconfig.js
+++ ormconfig.js
@@ -4,7 +4,7 @@ module.exports = [
         "host": process.env.DATABASE_HOST,
         "username": process.env.DATABASE_USERNAME,
         "password": process.env.DATABASE_PASSWORD,
-        "port": process.env.DATABASE_PORT,
+        "port": parseInt(process.env.DATABASE_PORT, 10),
         "database": process.env.DATABASE_DATABASE,
         "extra": {
             "options": {
